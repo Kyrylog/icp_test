9979026a3dac968b940360ab29575e0df5f48c23
Petro <belachka228@icloud.com>
2025-03-23 14:36:08 +0200

    init

--- src/hello_bachelor_rust_frontend/src/App.jsx
+++ src/hello_bachelor_rust_frontend/src/App.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react';
-import { hello_bachelor_rust_backend } from 'declarations/hello_bachelor_rust_backend';
+import { useState, useEffect } from "react";
+import { hello_bachelor_rust_backend } from "declarations/hello_bachelor_rust_backend";
+import { ethers } from "ethers";
 
 function App() {
-  const [greeting, setGreeting] = useState('');
+  const [greeting, setGreeting] = useState("");
+  const [rate, setRate] = useState("");
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -10,20 +12,52 @@ function App() {
     hello_bachelor_rust_backend.greet(name).then((greeting) => {
       setGreeting(greeting);
     });
+    hello_bachelor_rust_backend.get_icp_usd_exchange().then((bal)=>{
+      setRate(bal);
+
+    })
     return false;
   }
+  const [provider, setProvider] = useState(null);
+  const [bal, setBal] = useState(null);
+
+  useEffect(() => {
+    const initializeProvider = async () => {
+      if (window.ethereum) {
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+        const network = new ethers.Network("Whitechain Testnet", 2625);
+        const provider = new ethers.JsonRpcProvider(
+          "https://rpc-testnet.whitechain.io",
+          network,
+          { staticNetwork: network }
+        );
+        const balance = await provider.getBalance(
+          "0x4546F3147F094af3628531fA04FCE3347fC5D25D"
+        );
+        setBal(ethers.formatEther(balance));
+        setProvider(provider);
+      }
+    };
+
+    initializeProvider();
+  }, []);
 
   return (
     <main>
       <img src="/logo2.svg" alt="DFINITY logo" />
       <br />
       <br />
+      <div>ICP to USD prics: {greeting}</div>
+
       <form action="#" onSubmit={handleSubmit}>
         <label htmlFor="name">Enter your name: &nbsp;</label>
         <input id="name" alt="Name" type="text" />
         <button type="submit">Click Me!</button>
       </form>
-      <section id="greeting">{greeting}</section>
+      <section id="greeting">Greeting: {greeting}</section>
+      <section id="rate">Rate: {rate}</section>
+      <section id="balance">Balance: {bal}</section>
+
     </main>
   );
 }
